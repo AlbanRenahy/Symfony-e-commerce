2f57f6f9677c83a0c296cb713e9f2ec0b13664cf
AlbanRenahy <albanrenahy.pro@gmail.com>
2020-12-14 13:27:35 +0100

    Flash messages handling when a product is added to cart

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,21 +2,31 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
     /**
-     * @Route("/cart/add/{id}", name="cart_add")
+     * @Route("/cart/add/{id}", name="cart_add", requirements={"id":"\d+"})
      */
-    public function add($id, Request $request)
+    public function add($id, ProductRepository $productRepository, SessionInterface $session)
     {
+        // 0. Does the product exist in database
+        $product = $productRepository->find($id);
+
+        if(!$product){
+            throw $this->createNotFoundException("Le produit $id n'existe pas");
+        }
+
         // 1. Find the cart in session
         // 2. If doesn't exist, take an empty array
-        $cart = $request->getSession()->get('cart', []);
+        $cart = $session->get('cart', []);
 
         // 3. See if product(id) already exist in array
         // 4. If it's the case, simply increase the quantity
@@ -28,8 +38,17 @@ class CartController extends AbstractController
         }
         
         // 6. Register the updated array in session
-        $request->getSession()->set('cart', $cart);
+        $session->set('cart', $cart);
 
-        dd($request->getSession()->get('cart'));
+        /** @var FlashBag */
+        $flashBag = $session->getBag('flashes');
+
+        // Add flash messages
+        $flashBag->add('success', "Le produit a bien été ajouté au panier");
+
+        return $this->redirectToRoute('product_show', [
+            'category_slug' => $product->getCategory()->getSlug(),
+            'slug' => $product->getSlug()
+        ]);
     }
 }
